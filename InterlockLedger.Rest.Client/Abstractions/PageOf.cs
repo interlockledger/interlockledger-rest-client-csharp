5339358c41334d6d60fc6531fbd3358a938b7113
Rafael Teixeira <rteixeira@il2.io>
2022-05-11 15:01:43 -0300

    Copyright header fix

--- InterlockLedger.Rest.Client/Abstractions/PageOf.cs
+++ InterlockLedger.Rest.Client/Abstractions/PageOf.cs
@@ -1,6 +1,6 @@
 // ******************************************************************************************************************************
-//  
-// Copyright (c) 2018-2021 InterlockLedger Network
+//
+// Copyright (c) 2018-2022 InterlockLedger Network
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
